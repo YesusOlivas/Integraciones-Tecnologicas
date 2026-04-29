bf2f79416964a1d6aaa731dd54097f44c482a057
YesusOlivas <jesusantonioolivasflores31@gmail.com>
2026-04-29 09:12:28 -0600

    feat : (diseño de login aun sin funcionalidad)

--- src/app/demo/components/login/login/login.component.ts
+++ src/app/demo/components/login/login/login.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { RippleModule } from 'primeng/ripple';
+import { InputTextModule } from 'primeng/inputtext';
+import { CheckboxModule } from 'primeng/checkbox';
+import { AppConfigModule } from 'src/app/layout/config/app.config.module';
+import { PasswordModule } from 'primeng/password';
+import { LayoutService } from 'src/app/layout/service/app.layout.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ButtonModule,
+    RippleModule,
+    InputTextModule,
+    CheckboxModule,
+    AppConfigModule,
+    PasswordModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  confirmed: boolean = false;
 
+  constructor(public layoutService: LayoutService){}
 }
